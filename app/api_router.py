23132c3ff90bfabd88b663d096d9a9d8886e57ab
Hameed <wizrdcoder@gmail.com>
2026-02-08 20:16:37 +0000

    added gitignore

--- app/api_router.py
+++ app/api_router.py
@@ -8,7 +8,4 @@ api_router = APIRouter()
 # Include auth endpoints with prefix
 api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
 
-# Include other endpoints
-# api_router.include_router(users.router, prefix="/users", tags=["users"])
-
 api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
